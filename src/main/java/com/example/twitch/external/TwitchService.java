de76249d24a67a8b0cf21590d9d89799b2a965da
Long-Qin <mrmrmr.wayne@gmail.com>
2023-08-07 16:31:01 -0400

    Created Twitch client and service

---
+++ src/main/java/com/example/twitch/external/TwitchService.java
@@ -0,0 +1,46 @@
+package com.example.twitch.external;
+
+import com.example.twitch.external.model.*;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TwitchService {
+    private final TwitchApiClient twitchApiClient;
+
+    public TwitchService(TwitchApiClient twitchApiClient) {
+        this.twitchApiClient = twitchApiClient;
+    }
+
+    public List<Game> getTopGames() {
+        return twitchApiClient.getTopGames().data();
+    }
+
+    public List<Game> getGames(String name) {
+        return twitchApiClient.getGames(name).data();
+    }
+
+    public List<Stream> getStreams(List<Stream> gameIds, int first) {
+        return twitchApiClient.getStreams(gameIds, first).data();
+    }
+
+    public List<Video> getVideos(String gameIds, int first) {
+        return twitchApiClient.getVideos(gameIds, first).data();
+    }
+
+    public List<Clip> getClips(String gameIds, int first) {
+        return twitchApiClient.getClips(gameIds, first).data();
+    }
+
+    public List<String> getTopGameIds() throws IOException {
+        List<String> topGameIds = new ArrayList<>();
+        for (Game game : getTopGames()) {
+            topGameIds.add(game.id());
+        }
+
+        return topGameIds;
+    }
+}
