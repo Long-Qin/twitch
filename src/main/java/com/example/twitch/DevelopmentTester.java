8b5760c5e79379afa1f45a4e00008341403d46c6
Long-Qin <mrmrmr.wayne@gmail.com>
2023-08-08 19:32:41 -0400

    Did a test on database operations

--- src/main/java/com/example/twitch/DevelopmentTester.java
+++ src/main/java/com/example/twitch/DevelopmentTester.java
@@ -1,11 +1,15 @@
 package com.example.twitch;
 
 
-import com.example.twitch.external.model.Clip;
-import com.example.twitch.external.model.Game;
-import com.example.twitch.external.model.Stream;
+import com.example.twitch.db.FavoriteRecordRepository;
+import com.example.twitch.db.ItemRepository;
+import com.example.twitch.db.UserRepository;
+import com.example.twitch.db.entity.FavoriteRecordEntity;
+import com.example.twitch.db.entity.ItemEntity;
+import com.example.twitch.db.entity.UserEntity;
+import com.example.twitch.external.model.*;
 import com.example.twitch.external.TwitchService;
-import com.example.twitch.external.model.Video;
+import org.apache.catalina.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
@@ -13,37 +17,53 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 
+import java.time.Instant;
 import java.util.List;
 
 
 @Component
 public class DevelopmentTester implements ApplicationRunner {
 
-
     private static final Logger logger = LoggerFactory.getLogger(DevelopmentTester.class);
 
+    private final UserRepository userRepository;
+    private final ItemRepository itemRepository;
+    private final FavoriteRecordRepository favoriteRecordRepository;
 
-    private final TwitchService twitchService;
-
-
-    public DevelopmentTester(TwitchService twitchService) {
-        this.twitchService = twitchService;
+    public DevelopmentTester(UserRepository userRepository, ItemRepository itemRepository, FavoriteRecordRepository favoriteRecordRepository) {
+        this.userRepository = userRepository;
+        this.itemRepository = itemRepository;
+        this.favoriteRecordRepository = favoriteRecordRepository;
     }
 
-
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        List<Game> games = twitchService.getGames("fortnite");
-        List<Game> topGames = twitchService.getTopGames();
-        List<Video> videos = twitchService.getVideos("21779", 2);
-        List<Clip> clips = twitchService.getClips("21779", 2);
-        List<Stream> streams = twitchService.getStreams(List.of("18122", "21779", "33214"), 3);
+    public void run(ApplicationArguments args) {
+        UserEntity newUser = new UserEntity(null, "user0", "Foo", "Bar", "password");
+        userRepository.save(newUser);
 
+        UserEntity user1 = new UserEntity(null, "user1", "John", "Smith", "password");
+        UserEntity user2 = new UserEntity(null, "user2", "Jane", "Smith", "password");
+        UserEntity user3 = new UserEntity(null, "user3", "John", "Doe", "password");
 
-        logger.info(games.toString());
-        logger.info(topGames.toString());
-        logger.info(videos.toString());
-        logger.info(clips.toString());
-        logger.info(streams.toString());
+        userRepository.saveAll(List.of(user1, user2, user3));
+
+        ItemEntity newItem = new ItemEntity(null, "abc0", "Title0", "url0", "thumb0", "name0", "game0", ItemType.CLIP);
+        itemRepository.save(newItem);
+
+        List<ItemEntity> newItems = List.of(
+                new ItemEntity(null, "abc1", "Title1", "url1", "thumb1", "name1", "game1", ItemType.VIDEO),
+                new ItemEntity(null, "abc2", "Title2", "url2", "thumb2", "name2", "game2", ItemType.VIDEO),
+                new ItemEntity(null, "abc3", "Title3", "url3", "thumb3", "name3", "game3", ItemType.STREAM),
+                new ItemEntity(null, "abc4", "Title4", "url4", "thumb4", "name4", "game4", ItemType.STREAM)
+        );
+        itemRepository.saveAll(newItems);
+        favoriteRecordRepository.saveAll(
+                List.of(
+                        new FavoriteRecordEntity(null, 1L, 1L, Instant.now()),
+                        new FavoriteRecordEntity(null, 1L, 3L, Instant.now()),
+                        new FavoriteRecordEntity(null, 1L, 4L, Instant.now()),
+                        new FavoriteRecordEntity(null, 2L, 2L, Instant.now())
+                )
+        );
     }
 }
