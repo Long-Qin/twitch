b1aa5f4a322d8e548826e8cfe32c358ab8931e08
Long-Qin <mrmrmr.wayne@gmail.com>
2023-08-09 18:07:23 -0400

    minor change for the test case

--- src/main/java/com/example/twitch/DevelopmentTester.java
+++ src/main/java/com/example/twitch/DevelopmentTester.java
@@ -40,30 +40,5 @@ public class DevelopmentTester implements ApplicationRunner {
     public void run(ApplicationArguments args) {
         UserEntity newUser = new UserEntity(null, "user0", "Foo", "Bar", "password");
         userRepository.save(newUser);
-
-        UserEntity user1 = new UserEntity(null, "user1", "John", "Smith", "password");
-        UserEntity user2 = new UserEntity(null, "user2", "Jane", "Smith", "password");
-        UserEntity user3 = new UserEntity(null, "user3", "John", "Doe", "password");
-
-        userRepository.saveAll(List.of(user1, user2, user3));
-
-        ItemEntity newItem = new ItemEntity(null, "abc0", "Title0", "url0", "thumb0", "name0", "game0", ItemType.CLIP);
-        itemRepository.save(newItem);
-
-        List<ItemEntity> newItems = List.of(
-                new ItemEntity(null, "abc1", "Title1", "url1", "thumb1", "name1", "game1", ItemType.VIDEO),
-                new ItemEntity(null, "abc2", "Title2", "url2", "thumb2", "name2", "game2", ItemType.VIDEO),
-                new ItemEntity(null, "abc3", "Title3", "url3", "thumb3", "name3", "game3", ItemType.STREAM),
-                new ItemEntity(null, "abc4", "Title4", "url4", "thumb4", "name4", "game4", ItemType.STREAM)
-        );
-        itemRepository.saveAll(newItems);
-        favoriteRecordRepository.saveAll(
-                List.of(
-                        new FavoriteRecordEntity(null, 1L, 1L, Instant.now()),
-                        new FavoriteRecordEntity(null, 1L, 3L, Instant.now()),
-                        new FavoriteRecordEntity(null, 1L, 4L, Instant.now()),
-                        new FavoriteRecordEntity(null, 2L, 2L, Instant.now())
-                )
-        );
     }
 }
