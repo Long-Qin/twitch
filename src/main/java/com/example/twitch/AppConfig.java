61f7fdcdf30eee6955feb828fdda73cc4f8198ee
Long-Qin <mrmrmr.wayne@gmail.com>
2023-08-10 17:58:01 -0400

    Add JdbcUserManager and PasswordEncoder to secure user registration and password

--- src/main/java/com/example/twitch/AppConfig.java
+++ src/main/java/com/example/twitch/AppConfig.java
@@ -3,7 +3,13 @@ package com.example.twitch;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
@@ -15,5 +21,15 @@ public class AppConfig {
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/**").permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
